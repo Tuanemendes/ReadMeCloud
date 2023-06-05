1cb8f9f2d767c3263f63d52831dc0142ad60a3ce
Tuanemendes <tuane.belmiro@gmail.com>
2023-06-04 21:12:00 -0300

    implementação do botão gerar PDF

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,34 @@
 
 
 window.addEventListener('scroll', function () {
-    var header = document.querySelector('#header');
-    header.classList.toggle('scrolling', window.scrollY > 0);
+  var header = document.querySelector('#header');
+  header.classList.toggle('scrolling', window.scrollY > 0);
 
 });
 
 // Carousel
 var carousel = document.getElementById('carousel');
 var carouselInstance = new bootstrap.Carousel(carousel, {
-    interval: 6000,
-    slidesPerView: 3, 
-    wrap: true 
+  interval: 10000,
+  slidesPerView: 3,
+  wrap: true
 });
+
+
+
+function downloadPDF() {
+  var element = document.getElementById('container-pdf');
+
+  html2canvas(element).then(function (canvas) {
+    var imgData = canvas.toDataURL('image/png');
+    var doc = new jsPDF('p', 'mm', 'a4');
+
+    var margin = 13;
+
+    var imgWidth = doc.internal.pageSize.getWidth() - (margin * 2);
+    var imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+    doc.addImage(imgData, 'PNG', margin, margin, imgWidth, imgHeight);
+    doc.save('curriculo.pdf');
+  });
+}
